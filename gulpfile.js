8233873a0669564a18fb4a760e87de858436df14
weiguo.ni <neefrankie@gmail.com>
2015-10-27 17:08:03 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var mincss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var prefix = require('gulp-prefix');
 var rename = require('gulp-rename');
+var gulpif = require('gulp-if');
+var replace = require('gulp-replace');
 
 
 gulp.task('dist:test', function() {
@@ -17,11 +19,20 @@ gulp.task('html', function() {
 	return gulp.src('index.html')
 		.pipe(prefix(prefixUrl))
 		.pipe(rename('climate2015.html'))
+		.pipe(htmlmin({
+			removeComments: true,
+			collapseWhitespace: true,
+		}))
 		.pipe(gulp.dest('../deploy/dev_www/frontend/tpl/special/'));
 });
 
 gulp.task('copy', function() {
 	return gulp.src(['./*.{png,js,css,svg,csv}', '!gulpfile.js'])
+		.pipe(
+			gulpif('flatpack.js', 
+				replace('new.csv', 'http://www.ftchinese.com/m/marketing/climate.csv.html'
+			))
+		)
 		.pipe(gulp.dest('../ft-interact/climate-change-calculator/'));
 });
 
