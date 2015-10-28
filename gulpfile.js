393f925bed5adb06253d35d091e21a5a2455026f
weiguo.ni <neefrankie@gmail.com>
2015-10-28 14:56:20 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,11 @@ var prefix = require('gulp-prefix');
 var rename = require('gulp-rename');
 var gulpif = require('gulp-if');
 var replace = require('gulp-replace');
-var git = require('gulp-git');
-var runSequence = require('run-sequence');
-
 
 gulp.task('dist:test', function() {
 	return gulp.src(['./*.{png,js,css,svg,csv,html}', '!gulpfile.js']).
 		pipe(gulp.dest('../interact/climate-change-calculator'));
 });
-
-gulp.task('status', function() {
-	git.status(function(err, stdout) {
-		if (err) throw err;
-	});
-});
-
-gulp.task('add', function() {
-	return gulp.src('.')
-		.pipe(git.add())
-		.pipe(git.commit('dist commit'));
-});
-
-gulp.task('push', function() {
-	git.push('origin', 'chinese', function(err) {
-		if (err) {
-			console.log('`git push` encountered an error, please run `git push` manually.')
-			throw err;
-		}
-		console.log('Changes pushed to GitHub repository `chinese` branch!');
-	});
-});
-
 
 gulp.task('html', function() {
 	var prefixUrl = 'http://interactive.ftchinese.com/climate-change-calculator';
